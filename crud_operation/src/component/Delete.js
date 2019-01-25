6f3b68136c1d186e4ada631a389ce23f6ea9c324
Priyanka Mehta <priyanka.mehta@bacancytechnology.com>
2019-01-25 18:55:22 +0530

    Delete Complete

--- crud_operation/src/component/Delete.js
+++ crud_operation/src/component/Delete.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 class Delete extends Component {
 
   constructor(props) {
-
     super(props);
     this.state = {
       delete: false,
@@ -14,60 +13,43 @@ class Delete extends Component {
     };
     this.deleteRecord = this.deleteRecord.bind(this);
     this.isDelete = this.isDelete.bind(this);
-  
   }
 
   componentDidMount() {
-
     this.isDelete();
-  
   }
 
   deleteRecord() {
-
     axios.delete(`https://reqres.in/api/users/${this.props.match.params.id}`)
       .then((res) => {
-
         console.log('User deleted : ', res);
         this.setState({ delete: true });
-      
       }).catch((err) => {
-
         console.log("Error : ", err);
-      
       });
-  
   }
 
   isDelete() {
-
     console.log("dele");
     if (window.confirm('Are you sure you wish to delete this user?')) {
-
       this.deleteRecord();
-    
     } else {
-
       this.setState({ cancel: true });
-    
     }
-  
   }
 
   render() {
-
     return (
       <div>
         {this.state.delete ? <Redirect to='/' /> : null}
         {this.state.cancel ? <Redirect to='/' /> : null}
       </div>
     );
-
   }
-
 }
 
 Delete.propTypes = {
   match: PropTypes.object,
 };
+
 export default Delete;
