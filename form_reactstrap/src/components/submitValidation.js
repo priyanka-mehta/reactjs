8e6d1bd045024a360d08f3548ec1aa09162b118e
Priyanka Mehta <priyanka.mehta@bacancytechnology.com>
2019-02-22 12:47:30 +0530

    submit validation changes

--- form_reactstrap/src/components/submitValidation.js
+++ form_reactstrap/src/components/submitValidation.js
@@ -10,10 +10,16 @@ export const onSubmit = (signup, error) => {
   if (result) {
     return error;
   }
-  else if (!fields.name.match("[a-zA-Z]+\\.?")) {
+  else if (!fields.name.match(/^[a-zA-Z ]{2,30}$/)) {
     return error;
   }
-  else if (!fields.number.match("^[ ()+]*([0-9][ ()+]*){10}$")) {
+  else if (!fields.number.match(/^[ ()+]*([0-9][ ()+]*){10}$/)) {
+    return error;
+  }
+  else if(!fields.email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+    return error;
+  }
+  else if(!fields.password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)){
     return error;
   }
   else {
